2728b186d7bf609bcf3c37a946c774ddaed544f8
PabloRomeroAgudo <146949376+PabloRomeroAgudo@users.noreply.github.com>
2023-10-11 14:32:44 +0200

    Initial commit

---
+++ app/src/main/java/com/example/articulodecompose/MainActivity.kt
@@ -0,0 +1,76 @@
+package com.example.articulodecompose
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Surface
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.text.style.TextAlign
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+import com.example.articulodecompose.ui.theme.ArticuloDeComposeTheme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContent {
+            ArticuloDeComposeTheme {
+                // A surface container using the 'background' color from the theme
+                Surface(
+                    modifier = Modifier.fillMaxSize(),
+                    color = MaterialTheme.colorScheme.background
+                ) {
+                    Texto(stringResource(R.string.title), stringResource(R.string.firstText),
+                        stringResource(R.string.secondText))
+                }
+            }
+        }
+    }
+}
+
+@Composable
+fun Texto(title: String, firstText: String, secondText: String, modifier: Modifier = Modifier) {
+    val image = painterResource(R.drawable.bg_compose_background)
+    Column(modifier = modifier) {
+        Image(
+            painter = image,
+            contentDescription = null
+        )
+        Text(
+            text = title,
+            fontSize = 24.sp,
+            modifier = Modifier.padding(16.dp)
+        )
+        Text(
+            text = firstText,
+            modifier = Modifier.padding(start = 16.dp, end = 16.dp),
+            textAlign = TextAlign.Justify
+        )
+        Text(
+            text = secondText,
+            modifier = Modifier.padding(16.dp),
+            textAlign = TextAlign.Justify
+        )
+    }
+}
+
+@Preview(showBackground = true)
+@Composable
+fun GreetingPreview() {
+    ArticuloDeComposeTheme {
+        Texto(
+            title = stringResource(R.string.title), firstText = stringResource(R.string.firstText),
+            secondText = stringResource(R.string.secondText)
+        )
+    }
+}
